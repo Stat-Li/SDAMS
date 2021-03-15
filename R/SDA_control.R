919d149a89fb9be937d42b433060f40714cfa96e
Yuntong Li <li@li-2.local>
2021-03-15 12:25:20 -0400

    return beta and gamma as matrix

--- R/SDA_control.R
+++ R/SDA_control.R
@@ -15,17 +15,16 @@ SDA <- function(sumExp, VOI = NULL, ...){
     results <- Reduce('comb', rawresult)
     qv_1part <- apply(results$X1pvalue, 2, qvalue::qvalue,...)
     qv_2part <- qvalue::qvalue(results$X2pvalue,...)
-    nparams <- dim(results$pointest)[2]/2
-    df.results <- list(gamma = results$pointest[,1:nparams],
-                        beta = results$pointest[,-(1:nparams)],
-                        pv_gamma = results$X1pvalue[,1],
-                        pv_beta = results$X1pvalue[,2],
-                        qv_gamma = qv_1part[[1]]$qvalues,
-                        qv_beta = qv_1part[[2]]$qvalues,
+    nparams = dim(results$pointest)[2]/2
+    df.results <- list(gamma = as.matrix(results$pointest[,1:nparams]),
+                        beta = as.matrix(results$pointest[,-(1:nparams)]),
+                        pv_gamma = as.matrix(results$X1pvalue[,1]),
+                        pv_beta = as.matrix(results$X1pvalue[,2]),
+                        qv_gamma = as.matrix(qv_1part[[1]]$qvalues),
+                        qv_beta = as.matrix(qv_1part[[2]]$qvalues),
                         pv_2part = results$X2pvalue,
                         qv_2part = qv_2part$qvalues,
                         feat.names = feat.names)
     return(df.results)
-    return(results)
 }
 
